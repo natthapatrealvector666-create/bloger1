190b940dd2d3cc44d475f8a323b1c3ae94643dd6
unknown <natthapatrealvector666@gmail.com>
2026-09-14 22:15:13 +0700

    Fix db:seed error by removing User::factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ArticleTypeSeeder::class);
-
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
